0398c1cfb50aff4bc5eb81cf740f37fcb192e39d
SupU3 <javierguijarro2003@gmail.com>
2024-06-30 02:50:55 +0200

    ImGui added.

--- BBT/src/main.cpp
+++ BBT/src/main.cpp
@@ -1,14 +1,54 @@
 #include <iostream>
 #include "SDL3/SDL.h"
+#include "utils/imgui/imgui.h"
+#include "utils/imgui/imgui_impl_sdl3.h"
+#include "utils/imgui/imgui_impl_opengl3.h"
+#include "SDL3/SDL_opengl.h"
 
 int main()
 {
 
     SDL_Init(SDL_INIT_VIDEO);
 
+    const char* glsl_version = "#version 430";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG); // Always required on Mac
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+
+    // Enable native IME.
+    SDL_SetHint(SDL_HINT_IME_SHOW_UI, "1");
+
+    // Create window with graphics context
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    Uint32 window_flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN;
+
     SDL_Window* window;
 
-    window = SDL_CreateWindow("MORDI",600,400,0);
+    window = SDL_CreateWindow("BBT",600,400, window_flags);
+
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+
+    SDL_GL_MakeCurrent(window, glContext);
+
+    SDL_ShowWindow(window);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL3_InitForOpenGL(window, glContext);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+
 
     bool runing = true;
 
@@ -20,6 +60,8 @@ int main()
         while (SDL_PollEvent(&event))
         {
 
+            ImGui_ImplSDL3_ProcessEvent(&event);
+
             if (event.type == SDL_EVENT_QUIT)
             {
 
@@ -27,13 +69,37 @@ int main()
 
             }
 
+
+
         }
 
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
 
+        bool pOpen;
+        ImGui::ShowDemoWindow(&pOpen);
+
+        ImGui::Begin("Mordi");
+        ImGui::End();
+
+        ImGui::Render();
+        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+        glClearColor(0,0,0,0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
 
     }
 
     printf("Pichita");
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DeleteContext(glContext);
 
     return 0;
 
